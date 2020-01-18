09668788c4d5e493a77b815c2ff3a4988054804f
ardacetinkaya <ardacetinkaya@gmail.com>
2020-01-18 23:12:52 +0300

    E-mail

--- CostNotification/CostLibrary/FileAnalysis.cs
+++ CostNotification/CostLibrary/FileAnalysis.cs
@@ -6,11 +6,13 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
+    using System.Linq;
     using System.Threading.Tasks;
 
     public class FileAnalysis
     {
         private readonly ILogger _log;
+        private List<CostData> _fileContent = null;
 
         public FileAnalysis(ILogger log)
         {
@@ -19,53 +21,67 @@
 
         public async Task<List<CostData>> ReadFile(Stream file, string name)
         {
-            List<CostData> costs = null;
-
-            using var strmReader = new StreamReader(file);
-            using var csv = new CsvReader(strmReader, CultureInfo.InvariantCulture);
-            if (csv.Read())
+            try
             {
-                costs = new List<CostData>();
-
-                _log.LogInformation("Reading CSV");
-
-
-                csv.ReadHeader();
-                while (csv.Read())
+                using var strmReader = new StreamReader(file);
+                using var csv = new CsvReader(strmReader, CultureInfo.InvariantCulture);
+                if (csv.Read())
                 {
-                    var record = new CostData
+                    _fileContent = new List<CostData>();
+
+                    _log.LogInformation("Reading CSV");
+
+
+                    csv.ReadHeader();
+                    while (csv.Read())
                     {
+                        var record = new CostData
+                        {
 
-                        SubscriptionGuid = csv.GetField("SubscriptionGuid"),
-                        ResourceGroup = csv.GetField("ResourceGroup"),
-                        ResourceLocation = csv.GetField("ResourceLocation"),
-                        UsageDateTime = csv.GetField("UsageDateTime"),
-                        MeterCategory = csv.GetField("MeterCategory"),
-                        MeterSubcategory = csv.GetField("MeterSubcategory"),
-                        MeterId = csv.GetField("MeterId"),
-                        MeterName = csv.GetField("MeterName"),
-                        MeterRegion = csv.GetField("MeterRegion"),
-                        UsageQuantity = csv.GetField("UsageQuantity"),
-                        ResourceRate = csv.GetField("ResourceRate"),
-                        PreTaxCost = csv.GetField("PreTaxCost"),
-                        ConsumedService = csv.GetField("ConsumedService"),
-                        ResourceType = csv.GetField("ResourceType"),
-                        InstanceId = csv.GetField("InstanceId"),
-                        Tags = csv.GetField("Tags"),
-                        OfferId = csv.GetField("OfferId"),
-                        AdditionalInfo = csv.GetField("AdditionalInfo"),
-                        ServiceInfo1 = csv.GetField("ServiceInfo1"),
-                        ServiceInfo2 = csv.GetField("ServiceInfo2"),
-                        ServiceName = csv.GetField("ServiceName"),
-                        ServiceTier = csv.GetField("ServiceTier"),
-                        Currency = csv.GetField("Currency"),
-                        UnitOfMeasure = csv.GetField("UnitOfMeasure"),
-                    };
-                    costs.Add(record);
+                            SubscriptionGuid = csv.GetField("SubscriptionGuid"),
+                            ResourceGroup = csv.GetField("ResourceGroup"),
+                            ResourceLocation = csv.GetField("ResourceLocation"),
+                            UsageDateTime = csv.GetField("UsageDateTime"),
+                            MeterCategory = csv.GetField("MeterCategory"),
+                            MeterSubcategory = csv.GetField("MeterSubcategory"),
+                            MeterId = csv.GetField("MeterId"),
+                            MeterName = csv.GetField("MeterName"),
+                            MeterRegion = csv.GetField("MeterRegion"),
+                            UsageQuantity = csv.GetField("UsageQuantity"),
+                            ResourceRate = csv.GetField("ResourceRate"),
+                            PreTaxCost = csv.GetField("PreTaxCost"),
+                            ConsumedService = csv.GetField("ConsumedService"),
+                            ResourceType = csv.GetField("ResourceType"),
+                            InstanceId = csv.GetField("InstanceId"),
+                            Tags = csv.GetField("Tags"),
+                            OfferId = csv.GetField("OfferId"),
+                            AdditionalInfo = csv.GetField("AdditionalInfo"),
+                            ServiceInfo1 = csv.GetField("ServiceInfo1"),
+                            ServiceInfo2 = csv.GetField("ServiceInfo2"),
+                            ServiceName = csv.GetField("ServiceName"),
+                            ServiceTier = csv.GetField("ServiceTier"),
+                            Currency = csv.GetField("Currency"),
+                            UnitOfMeasure = csv.GetField("UnitOfMeasure"),
+                        };
+                        _fileContent.Add(record);
 
+                    }
                 }
             }
-            return costs;
+            catch (Exception ex)
+            {
+                _log.LogError(ex, $"Unable to parse file. {ex.Message}");
+            }
+
+            return _fileContent;
+        }
+
+        public void AnalyseCost()
+        {
+            if(_fileContent!=null)
+            {
+                
+            }
         }
     }
 }
--- CostNotification/NotificationFunction/Notification.cs
+++ CostNotification/NotificationFunction/Notification.cs
@@ -3,6 +3,7 @@ namespace NotificationFunction
     using CostLibrary;
     using Microsoft.Azure.WebJobs;
     using Microsoft.Extensions.Logging;
+    using SendGrid.Helpers.Mail;
     using System;
     using System.IO;
     using System.Threading.Tasks;
@@ -11,20 +12,33 @@ namespace NotificationFunction
     {
 
         [FunctionName("Notification")]
-        public async static Task Run([BlobTrigger("cost/report/{name}", Connection = "StorageConnection")]Stream myBlob, string name, ILogger log)
+        public async static Task Run([BlobTrigger("cost/report/{name}", Connection = "StorageConnection")]Stream myBlob, string name,
+            [SendGrid(ApiKey = "SendGridKeyAppSettingName")] IAsyncCollector<SendGridMessage> messageCollector,ILogger log)
         {
-            
+
             try
             {
                 FileAnalysis fa = new FileAnalysis(log);
 
                 var contents = await fa.ReadFile(myBlob, name);
+
+                var output = contents.ToStringTable(
+                    new[] { "Service Name", "Resource Group", "Meter Category" },
+                    a => a.ServiceName, a => a.ResourceGroup, a => a.MeterCategory);
+
+                var message = new SendGridMessage();
+                message.AddTo("");
+                message.AddContent("text/html", output);
+                message.SetFrom(new EmailAddress("noreply@productx.com"));
+                message.SetSubject("Cost Analysis");
+
+                await messageCollector.AddAsync(message);
             }
             catch (Exception ex)
             {
                 log.LogError(ex, ex.Message);
             }
-       
+
 
         }
     }
